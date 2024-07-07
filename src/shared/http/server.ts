7a30a2dc8d078243da4e3f108daded64bf42fad1
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-07-06 21:02:56 -0300

    first commit

---
+++ src/shared/http/server.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: error.message,
+  });
+});
+
+app.listen(3333, () => {
+  console.log(
+    `Server started on porta 3333!,
+acesse o link: http://localhost:3333`,
+  );
+});
