99b20cc45a5ae6a651954b7257e41a0794cec8c1
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-07-07 11:19:59 -0300

    repositorio personalizado

---
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from "typeorm";
+import Product from '../entities/Product';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    return this.findOne({
+      where: {
+        name,
+      },
+    });
+  }
+}
