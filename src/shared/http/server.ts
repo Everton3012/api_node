48b856713a30310fc61ad2d6b942125a3f641ce5
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-07-16 16:57:00 -0300

    modulo de usuarios e autenticado

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,37 +1,38 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors'
+import 'express-async-errors';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
       status: 'error',
       message: 'Internal server error',
     });
-  }
-  return res.status(500).json({
-    status: 'error',
-    message: error.message,
-  });
-});
+  },
+);
 
 app.listen(3333, () => {
-  console.log(
-    `Server started on porta 3333!,
-acesse o link: http://localhost:3333`,
-  );
+  console.log('Server started on port 3333! 🏆');
 });
